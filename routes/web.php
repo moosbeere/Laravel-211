7ffffbbf6b8c127663aeb3ab0e56e1f99fa3ec01
moosbeere <moosbeere_O@mail.ru>
2022-11-02 20:58:38 +0300

    lab1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/layout');
+});
+
+Route::get('/about', function () {
+    return view('main/about');
+});
+
+Route::get('/contact', function () {
+    return view('main/adress');
 });
