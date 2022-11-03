02dafbd0404398a5e1a9641df75100d9fd28120c
moosbeere <moosbeere_O@mail.ru>
2022-11-03 06:40:20 +0300

    db

---
+++ database/migrations/2022_11_03_032626_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->binary('preview_image');
+            $table->binary('full_image');
+            $table->string('shortDesc')->nullable();
+            $table->string('desc'); 
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
