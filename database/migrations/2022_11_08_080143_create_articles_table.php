831ed13b87a3dd6da55d51fb4a956a62c00a318c
moosbeere <moosbeere_O@mail.ru>
2022-11-08 11:56:12 +0300

    model seed json

---
+++ database/migrations/2022_11_08_080143_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('name');
+            $table->string('preview_image');
+            $table->string('full_image');
+            $table->string('shortDesc')->nullable();
+            $table->string('desc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
