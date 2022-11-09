9736f49f1b4bb795dd7f3d9c79f6d5103c3a02c1
moosbeere <moosbeere_O@mail.ru>
2022-11-09 10:30:01 +0300

    Lab2_1

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts.layout')
 @section('content')
-    <p>Hello, World!</p>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Title</th>
+      <th scope="col">Desc</th>
+      <th scope="col">Image</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($articles as $article)
+    <tr>
+      <th scope="row">{{$article['date']}}</th>
+      <td>{{$article['name']}}</td>
+      <td>{{$article['desc']}}</td>
+      <td><img src="{{URL::asset($article['preview_image'])}}" alt="" height="100" width="100"></td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
 @endsection
