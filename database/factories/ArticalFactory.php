768324241d18483943cfbfe423d48417dc427c46
moosbeere <moosbeere_O@mail.ru>
2022-11-15 09:39:26 +0300

    1

---
+++ database/factories/ArticalFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'date' => $this->faker->date(),
+            'name' => $this->faker->word(),
+            
+
+        ];
+    }
+}
